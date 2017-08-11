44cef0c9e5a57d02f5dfa3615d051a03ef85cead
Thomas Neumeier <work@th-neumeier.de>
2017-08-12 00:13:56 +0200

    create server module

---
+++ server/src/main/java/hundsfickerei/schafkopf/server/ServerApplication.java
@@ -0,0 +1,12 @@
+package hundsfickerei.schafkopf.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerApplication.class, args);
+	}
+}
