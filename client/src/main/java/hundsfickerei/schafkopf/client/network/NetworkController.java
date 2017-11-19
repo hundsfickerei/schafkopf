a043bc5a3673f847e9fb286292615f961dbf4598
rvalentini <riccardo.valentini@comsysto.com>
2017-11-19 15:04:31 +0100

    extended functionality of NetworkController and added proper logging

--- client/src/main/java/hundsfickerei/schafkopf/client/network/NetworkController.java
+++ client/src/main/java/hundsfickerei/schafkopf/client/network/NetworkController.java
@@ -1,11 +1,18 @@
 package hundsfickerei.schafkopf.client.network;
 
+
 import org.zeromq.ZContext;
 import org.zeromq.ZMQ;
 import java.util.UUID;
 import java.util.Random;
+import java.util.logging.Logger;
+import static java.util.logging.Level.*;
+
 
 public class NetworkController  {
+
+    private final static Logger LOG = Logger.getLogger(IncomingMessageHandler.class.getSimpleName());
+
 
     private static Random rand = new Random(System.nanoTime());
 
@@ -13,8 +20,13 @@ public class NetworkController  {
 
     private String identity;
 
+    private IncomingMessageHandler incomingMessageHandler;
+    private OutgoingMessageHandler outgoingMessageHandler;
+
     public NetworkController() {
         this.identity = UUID.randomUUID().toString();
+        this.incomingMessageHandler = new IncomingMessageHandler();
+        this.outgoingMessageHandler = new OutgoingMessageHandler(this);
         initialize();
     }
 
@@ -24,14 +36,31 @@ public class NetworkController  {
         upChannel.setIdentity(identity.getBytes());
         upChannel.connect("tcp://localhost:5570");
 
-        ServerConnectionWorker worker = new ServerConnectionWorker(ctx, upChannel, identity);
+        ServerConnectionWorker worker = new ServerConnectionWorker(ctx, this);
         new Thread(worker).start();
 
     }
 
-    public void sendToServer(String msg){
+    public void sendMessageToServer(String msg){
         upChannel.send(msg,0);
     }
 
+    public void receiveMessageFromServer(String msg){
+        LOG.log(INFO,"-------------------");
+        LOG.log(INFO, "Received new message from server!");
+        LOG.log(INFO,"Payload: " + msg);
+        incomingMessageHandler.processMessage(msg);
+    }
 
+    public ZMQ.Socket getUpChannel() {
+        return upChannel;
+    }
+
+    public String getIdentity() {
+        return identity;
+    }
+
+    public OutgoingMessageHandler getOutgoingMessageHandler() {
+        return outgoingMessageHandler;
+    }
 }
